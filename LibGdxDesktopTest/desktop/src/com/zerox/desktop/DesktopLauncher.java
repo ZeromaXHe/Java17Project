f1323467235a83aa25d68c0c4b79b96f950e55e7
Zhu XH <zhuxiaohe95@163.com>
2022-06-20 17:50:50 +0800

    feature (LibGdxDesktopTest): LibGdx 生成的 Desktop + Html 项目 包含 Bullet、Freetype 以外的全部官方扩展，LibGDX 版本为 1.11.0

---
+++ LibGdxDesktopTest/desktop/src/com/zerox/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.zerox.desktop;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.zerox.desktop.LibGdxDesktopTest;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("LibGdxDesktopTest");
+		new Lwjgl3Application(new LibGdxDesktopTest(), config);
+	}
+}
