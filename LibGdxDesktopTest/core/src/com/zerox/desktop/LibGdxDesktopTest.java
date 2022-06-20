5290e2da1b5f8948ca8b7b4e71cbca37ae689773
Zhu XH <zhuxiaohe95@163.com>
2022-06-20 18:47:09 +0800

    feature (LibGdxDesktopTest): 官方 wiki - A Simple Game

--- LibGdxDesktopTest/core/src/com/zerox/desktop/LibGdxDesktopTest.java
+++ LibGdxDesktopTest/core/src/com/zerox/desktop/LibGdxDesktopTest.java
@@ -1,31 +1,131 @@
 package com.zerox.desktop;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.TimeUtils;
+
+import java.util.Iterator;
 
 public class LibGdxDesktopTest extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+    private Texture dropImage;
+    private Texture bucketImage;
+    private Sound explodeSound;
+    private Music tankMusic;
 
-	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    private OrthographicCamera camera;
+    private SpriteBatch batch;
+    private Rectangle bucket;
+    private Array<Rectangle> raindrops;
+    private long lastDropTime;
+
+    @Override
+    public void create() {
+        // 加载水滴和桶的图片，都是 64 * 64 像素的
+        dropImage = new Texture(Gdx.files.internal("drop.png"));
+        bucketImage = new Texture(Gdx.files.internal("bucket.png"));
+
+        // 加载爆炸音效和坦克大战背景音乐（不想登录去下那个素材了，用原来自己学 FXGL 的素材糊弄一下……）
+        explodeSound = Gdx.audio.newSound(Gdx.files.internal("explode.wav"));
+        tankMusic = Gdx.audio.newMusic(Gdx.files.internal("tankBgm.mp3"));
+
+        // 立即启动背景音乐的播放
+        tankMusic.setLooping(true);
+        tankMusic.play();
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 480);
+
+        batch = new SpriteBatch();
+
+        bucket = new Rectangle();
+        bucket.x = 800 / 2 - 64 / 2;
+        // libGDX (以及 OpenGL) 中的 y 轴是朝上的
+        bucket.y = 20;
+        bucket.width = 64;
+        bucket.height = 64;
+
+        raindrops = new Array<>();
+        spawnRaindrop();
+    }
+
+    @Override
+    public void render() {
+        ScreenUtils.clear(0, 0, 0.2f, 1);
+
+        camera.update();
+
+        batch.setProjectionMatrix(camera.combined);
+        batch.begin();
+        batch.draw(bucketImage, bucket.x, bucket.y);
+        for (Rectangle raindrop : raindrops) {
+            batch.draw(dropImage, raindrop.x, raindrop.y);
+        }
+        batch.end();
+
+        if (Gdx.input.isTouched()) {
+            Vector3 touchPos = new Vector3();
+            touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+            camera.unproject(touchPos);
+            bucket.x = touchPos.x - 64 / 2;
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            bucket.x -= 200 * Gdx.graphics.getDeltaTime();
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            bucket.x += 200 * Gdx.graphics.getDeltaTime();
+        }
+
+        if (bucket.x < 0) {
+            bucket.x = 0;
+        }
+        if (bucket.x > 800 - 64) {
+            bucket.x = 800 - 64;
+        }
+
+        if (TimeUtils.nanoTime() - lastDropTime > 1000000000) {
+            spawnRaindrop();
+        }
+        for (Iterator<Rectangle> iter = raindrops.iterator(); iter.hasNext(); ) {
+            Rectangle raindrop = iter.next();
+            raindrop.y -= 200 * Gdx.graphics.getDeltaTime();
+            if (raindrop.y + 64 < 0) {
+                iter.remove();
+            }
+            if (raindrop.overlaps(bucket)) {
+                explodeSound.play();
+                iter.remove();
+            }
+        }
+    }
+
+    private void spawnRaindrop() {
+        Rectangle raindrop = new Rectangle();
+        raindrop.x = MathUtils.random(0, 800 - 64);
+        raindrop.y = 480;
+        raindrop.width = 64;
+        raindrop.height = 64;
+        raindrops.add(raindrop);
+        lastDropTime = TimeUtils.nanoTime();
+    }
+
+    @Override
+    public void dispose() {
+        dropImage.dispose();
+        bucketImage.dispose();
+        explodeSound.dispose();
+        tankMusic.dispose();
+        batch.dispose();
+    }
 }
--- LibGdxDesktopTest/desktop/src/com/zerox/desktop/DesktopLauncher.java
+++ LibGdxDesktopTest/desktop/src/com/zerox/desktop/DesktopLauncher.java
@@ -6,10 +6,12 @@ import com.zerox.desktop.LibGdxDesktopTest;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setForegroundFPS(60);
-		config.setTitle("LibGdxDesktopTest");
-		new Lwjgl3Application(new LibGdxDesktopTest(), config);
-	}
+    public static void main(String[] arg) {
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setWindowedMode(800, 480);
+        config.useVsync(true);
+        config.setForegroundFPS(60);
+        config.setTitle("LibGdxDesktopTest");
+        new Lwjgl3Application(new LibGdxDesktopTest(), config);
+    }
 }
